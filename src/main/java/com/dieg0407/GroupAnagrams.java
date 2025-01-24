5aeec497f377ec7feda3ea38533eb1f6af6fb5e5
Dieg0407 <diego.pastor.0407@gmail.com>
2025-01-24 18:33:20 -0500

    chore: added group anagrams

---
+++ src/main/java/com/dieg0407/GroupAnagrams.java
@@ -0,0 +1,25 @@
+package com.dieg0407;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * ref: https://neetcode.io/problems/anagram-groups
+ */
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        var hashMap = new HashMap<String, List<String>>();
+        for (var str : strs) {
+            var chars = new int[26];
+            for (var character : str.toCharArray()) {
+                chars[character - 'a']++;
+            }
+            var key = Arrays.toString(chars);
+            hashMap.putIfAbsent(key, new ArrayList<>());
+            hashMap.get(key).add(str);
+        }
+        return new ArrayList<>(hashMap.values());
+    }
+}
---
+++ src/test/java/com/dieg0407/GroupAnagramsTest.java
@@ -0,0 +1,36 @@
+package com.dieg0407;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class GroupAnagramsTest {
+
+    private final GroupAnagrams solution = new GroupAnagrams();
+
+    @Test
+    void example1() {
+        var strs = new String[] { "act", "pots", "tops", "cat", "stop", "hat" };
+        assertThat(solution.groupAnagrams(strs))
+                .containsExactlyInAnyOrder(
+                        Arrays.asList("hat"),
+                        Arrays.asList("act", "cat"),
+                        Arrays.asList("pots", "tops", "stop"));
+    }
+
+    @Test
+    void example2() {
+        var strs = new String[] { "x" };
+        assertThat(solution.groupAnagrams(strs))
+                .containsExactlyInAnyOrder(Arrays.asList("x"));
+    }
+
+    @Test
+    void example3() {
+        var strs = new String[] { "" };
+        assertThat(solution.groupAnagrams(strs))
+                .containsExactlyInAnyOrder(Arrays.asList(""));
+
+    }
+}
